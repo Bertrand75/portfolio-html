64779e4c07eac816c11006abba5b09c8a97348ee
BertrandEPSI <138715108+BertrandEPSI@users.noreply.github.com>
2024-08-15 23:05:54 +0200

    amélioration slider + ajout images projets CRM

--- js/slider.js
+++ js/slider.js
@@ -8,8 +8,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
     var i;
-    var slides = document.getElementsByClassName("slide");
-    var dots = document.getElementsByClassName("dot");
+
     if (n > slides.length) {
         slideIndex = 1;
     }
@@ -19,11 +18,25 @@ function showSlides(n) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
+
+    var dots = document.getElementsByClassName("dot");
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
+}
+
+var domSlideIndexes = document.getElementsByClassName("slide-index");
+for (let i = 0; i < domSlideIndexes.length; i++) {
+    domSlideIndexes[i].innerHTML = i + 1 + " / " + domSlideIndexes.length;
+}
+
+var slides = document.getElementsByClassName("slide");
+let dotsContainer = document.getElementsByClassName("slide-dot");
+for (i = 1; i <= slides.length; i++) {
+    dotsContainer[0].innerHTML +=
+        '<span class="dot" onclick="currentSlide(' + i + ')"></span>';
 }
 
 var slideIndex = 1;
