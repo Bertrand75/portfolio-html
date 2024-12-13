9e02ff6ec93aaa1cbb59ef63d52a2128f9a0b435
Bertrand <65681652+Bertrand75@users.noreply.github.com>
2024-12-13 19:02:07 +0100

    Ajout projet galerie d'art

--- js/slider.js
+++ js/slider.js
@@ -1,14 +1,11 @@
 function plusSlides(n) {
     showSlides((slideIndex += n));
 }
-
 function currentSlide(n) {
     showSlides((slideIndex = n));
 }
-
 function showSlides(n) {
     var i;
-
     if (n > slides.length) {
         slideIndex = 1;
     }
@@ -18,7 +15,6 @@ function showSlides(n) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-
     var dots = document.getElementsByClassName("dot");
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
@@ -26,18 +22,15 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
-
 var domSlideIndexes = document.getElementsByClassName("slide-index");
 for (let i = 0; i < domSlideIndexes.length; i++) {
     domSlideIndexes[i].innerHTML = i + 1 + " / " + domSlideIndexes.length;
 }
-
 var slides = document.getElementsByClassName("slide");
 let dotsContainer = document.getElementsByClassName("slide-dot");
 for (i = 1; i <= slides.length; i++) {
     dotsContainer[0].innerHTML +=
         '<span class="dot" onclick="currentSlide(' + i + ')"></span>';
 }
-
 var slideIndex = 1;
 showSlides(slideIndex);
